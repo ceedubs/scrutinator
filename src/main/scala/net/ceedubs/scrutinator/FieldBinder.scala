d04923d6e3a172b09e3c758782f946e9c52f0fdd
Cody Allen <ceedubs@gmail.com>
2014-03-24 16:09:31 -0400

    Add FieldBindingStrategy

--- src/main/scala/net/ceedubs/scrutinator/FieldBinder.scala
+++ src/main/scala/net/ceedubs/scrutinator/FieldBinder.scala
@@ -16,40 +16,53 @@ trait FieldBinder[L <: HList, I] {
   def apply(fields: L): Kleisli[ErrorsOr, I, R]
 }
 
-object FieldBinder {
-
-  object tuplesToFields extends Poly1 {
-    implicit def atTuple[K, V] = at[(K, V)](Function.tupled((key, value) => field[K](value)))
-  }
-
-  object bindParam extends Poly1 {
-    implicit def atField[K, A, O](implicit npc: NamedParamConverter[K], reader: ParamReader[ErrorsOr, (NamedParam[A], Request), O]) = at[FieldType[K, A]] { param =>
-      val namedParam: NamedParam[A] = npc.asNamedParam(param)
-      reader.reader.local((request: Request) => (namedParam, request))
-    }
-  }
+object FieldBinder extends RequestFieldBinders {
 
   type Aux[L <: HList, I, R0 <: HList] = FieldBinder[L, I] {
     type R = R0
   }
 
-  implicit def fieldBinder[L <: HList, K <: HList, I, O1 <: HList, F0[_], O2 <: HList, F1[_], O3 <: HList, O4 <: HList](implicit
+}
+
+object tuplesToFields extends Poly1 {
+  implicit def atTuple[K, V] = at[(K, V)](Function.tupled((key, value) => field[K](value)))
+}
+
+trait FieldBindingStrategy[L <: HList, I, P <: Poly] {
+  type R <: HList
+
+  def fieldBinder: FieldBinder.Aux[L, I, R]
+
+  def apply(fields: L): Kleisli[ErrorsOr, I, R] = fieldBinder(fields)
+}
+
+object FieldBindingStrategy {
+
+  type Aux[L <: HList, I, P <: Poly, R0 <: HList] = FieldBindingStrategy[L, I, P] {
+    type R = R0
+  }
+
+  implicit def polyTraverserStrategy[L <: HList, K <: HList, I, P <: Poly, O1 <: HList, F0[_], O2 <: HList, F1[_], O3 <: HList, O4 <: HList](implicit
       keys: Keys.Aux[L, K],
-      bindParamTraverser: TraverserAux[L, bindParam.type, F0, O1],
+      bindParamTraverser: TraverserAux[L, P, F0, O1],
       fEv: UnapplyAux[Functor, F0[O1], F1, O2],
       zipper: shapeless.ops.hlist.Zip.Aux[K :: O2 :: HNil, O3],
       fieldMapper: Mapper.Aux[tuplesToFields.type, O3, O4],
-      lEv: F1[O4] === Kleisli[ErrorsOr, I, O4]): FieldBinder.Aux[L, I, O4] = new FieldBinder[L, I] {
+      lEv: F1[O4] === Kleisli[ErrorsOr, I, O4]): FieldBindingStrategy.Aux[L, I, P, O4] = new FieldBindingStrategy[L, I, P] {
 
     type R = O4
 
-    def apply(fields: L): Kleisli[ErrorsOr, I, O4] = {
-      val bindingsReader = bindParamTraverser(fields)
-      val result = fEv.TC.map(fEv(bindingsReader)) { boundValues =>
-        val tupled = zipper(keys() :: boundValues :: HNil) 
-        fieldMapper(tupled)
+    val fieldBinder = new FieldBinder[L, I] {
+      type R = O4
+
+      def apply(fields: L): Kleisli[ErrorsOr, I, O4] = {
+        val bindingsReader = bindParamTraverser(fields)
+        val result = fEv.TC.map(fEv(bindingsReader)) { boundValues =>
+          val tupled = zipper(keys() :: boundValues :: HNil) 
+          fieldMapper(tupled)
+        }
+        witness(lEv)(result)
       }
-      witness(lEv)(result)
     }
   }
 }
---
+++ src/main/scala/net/ceedubs/scrutinator/RequestFieldBinders.scala
@@ -0,0 +1,16 @@
+package net.ceedubs.scrutinator
+
+import net.ceedubs.scrutinator.readers.ParamReader
+import shapeless._
+import shapeless.record._
+
+trait RequestFieldBinders {
+  implicit def requestFieldBinder[L <: HList](implicit strategy: FieldBindingStrategy[L, Request, bindParamFromRequest.type]): FieldBinder.Aux[L, Request, strategy.R] = strategy.fieldBinder
+}
+
+object bindParamFromRequest extends Poly1 {
+  implicit def atField[K, A, O](implicit npc: NamedParamConverter[K], reader: ParamReader[ErrorsOr, (NamedParam[A], Request), O]) = at[FieldType[K, A]] { param =>
+    val namedParam: NamedParam[A] = npc.asNamedParam(param)
+    reader.reader.local((request: Request) => (namedParam, request))
+  }
+}
